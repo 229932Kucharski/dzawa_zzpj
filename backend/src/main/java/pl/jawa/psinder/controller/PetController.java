7354d68b02c6b23d1d0c705804a8d29541e7ed99
Krzysztof Dwużnik <krzysztofd_1999@wp.pl>
2022-05-03 21:04:27 +0200

    added get pet by ID

--- backend/src/main/java/pl/jawa/psinder/controller/PetController.java
+++ backend/src/main/java/pl/jawa/psinder/controller/PetController.java
@@ -1,6 +1,5 @@
 package pl.jawa.psinder.controller;
 
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +8,7 @@ import pl.jawa.psinder.entity.Pet;
 import pl.jawa.psinder.repository.PetRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pets")
@@ -20,15 +20,20 @@ public class PetController {
         this.petRepository = petRepository;
     }
 
+    //get all pets from petRepository
     @GetMapping("")
     public List<Pet> getPets() {
         return petRepository.findAll();
     }
 
+    //get all pets from user given by id
     @GetMapping("?userid={id}")
     public List<Pet> getPetsByUserId(@PathVariable long id) {
         return petRepository.findByUserId(id);
     }
 
-
+    @GetMapping("?id={id}")
+    public Optional<Pet> getPetById(@PathVariable long id) {
+        return petRepository.findById(id);
+    }
 }
