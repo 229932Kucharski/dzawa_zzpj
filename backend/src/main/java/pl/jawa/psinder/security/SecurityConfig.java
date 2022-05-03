0d5e6fec2e8e5ee7c5ca3d75a4b51c7b67579389
Gelo <gelo2424@wp.pl>
2022-05-03 16:26:44 +0200

    LoginController /user endpoint, secured /pets endpoint

--- backend/src/main/java/pl/jawa/psinder/security/SecurityConfig.java
+++ backend/src/main/java/pl/jawa/psinder/security/SecurityConfig.java
@@ -11,6 +11,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/", "/login").permitAll()
+                    .anyRequest().authenticated();
     }
 }
