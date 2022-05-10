5484f5e30db153fc7cf7b9a5248196e55b3fd62f
Krzysztof Dwużnik <krzysztofd_1999@wp.pl>
2022-05-10 22:15:49 +0200

    Added get pets with filters

--- backend/src/main/java/pl/jawa/psinder/controller/PetController.java
+++ backend/src/main/java/pl/jawa/psinder/controller/PetController.java
@@ -1,12 +1,13 @@
 package pl.jawa.psinder.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import pl.jawa.psinder.entity.Pet;
 import pl.jawa.psinder.entity.User;
 import pl.jawa.psinder.repository.PetRepository;
+import pl.jawa.psinder.webclient.AddressDistanceClient;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,8 +28,8 @@ public class PetController {
 
     //get pet
     @GetMapping("/pets/{id}")
-    public Optional<Pet> getPetById(@PathVariable long id) {
-        return petRepository.findById(id);
+    public Pet getPetById(@PathVariable long id) {
+        return petRepository.getById(id);
     }
 
     //create pet
@@ -50,11 +51,38 @@ public class PetController {
         petRepository.deleteById(id);
     }
 
-//    //get pet with filters
-//    @GetMapping("/pets")
-//    public getFilteredPets(@Param()) {
-//
-//    }
+    //get pet with filters
+    @GetMapping("/pets/filtered")
+    public List<Pet> getFilteredPets(@Param("race") String race, @Param("size") String size, @Param("city") String city, @Param("street") String street, @Param("distance") double distance) {
+        List<Pet> result = petRepository.findAll();
+        if (race != null) {
+            result = result.stream().filter(pet -> pet.getRace().equals(race)).toList();
+        }
+        if (size != null) {
+            result = result.stream().filter(pet -> pet.getSize().equals(size)).toList();
+        }
+        if (city !=null) {
+            if (street !=null) {
+                String address = street + ", " + city;
+                AddressDistanceClient client = new AddressDistanceClient();
+                result = result.stream().filter(pet ->
+                        {
+                            String tmp = pet.getAddress().getStreet()
+                                    + ", " + pet.getAddress().getCity();
+                            try {
+                                double dist = client.getDistance(address, tmp);
+                                return dist <= distance;
+                            } catch (JsonProcessingException e) {
+                                return false;
+                            }
+                        }).toList();
+            } else {
+                result = result.stream().filter(pet -> pet.getAddress().getCity().equals(city)).toList();
+            }
+        }
+        return result;
+    }
+
     //get all pets from user given by id
     //should be in user
     @GetMapping("/users/{id}/pets")
