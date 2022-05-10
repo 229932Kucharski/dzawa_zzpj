bec6aa4222ee8a68a858205a711700e3cfec7c99
Krzysztof Dwużnik <krzysztofd_1999@wp.pl>
2022-05-10 21:09:23 +0200

    Added create, update, delete pet

--- backend/src/main/java/pl/jawa/psinder/controller/PetController.java
+++ backend/src/main/java/pl/jawa/psinder/controller/PetController.java
@@ -1,5 +1,6 @@
 package pl.jawa.psinder.controller;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import pl.jawa.psinder.entity.Pet;
 import pl.jawa.psinder.entity.User;
@@ -10,7 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/pets")
 public class PetController {
 
     private final PetRepository petRepository;
@@ -20,30 +20,52 @@ public class PetController {
     }
 
     //get all pets from petRepository
-    @GetMapping("")
+    @GetMapping("/pets")
     public List<Pet> getPets() {
         return petRepository.findAll();
     }
 
-    //get all pets from user given by id
-    @GetMapping("/user")
-    public List<Pet> getPetsByUserId(@RequestParam("id") long id) {
-        return petRepository.findByUserId(id);
-    }
-
-    //get pet by given id
-    @GetMapping("/{id}")
+    //get pet
+    @GetMapping("/pets/{id}")
     public Optional<Pet> getPetById(@PathVariable long id) {
         return petRepository.findById(id);
     }
 
-    //get user info from pet id
-    @GetMapping("/users")
-    public List<String> getPetParentById(@RequestParam("pet_id") long id) {
-        List<String> result = new ArrayList<>();
-        User temp = petRepository.getById(id).getUser();
-        result.add(temp.getUsername());
-        result.add(String.valueOf(temp.getId()));
-        return result;
+    //create pet
+    @PostMapping("/pets")
+    public Pet createPet(@RequestBody Pet pet) {
+        return petRepository.save(pet);
     }
+
+    //update pet
+    @PutMapping("/pets/{id}")
+    public Pet UpdatePet(@PathVariable long id, @RequestBody Pet pet) {
+        pet.setId(id);
+        return petRepository.save(pet);
+    }
+
+    //delete pet with given id
+    @DeleteMapping("/pets/{id}")
+    public void deletePet(@PathVariable long id) {
+        petRepository.deleteById(id);
+    }
+
+//    //get pet with filters
+//    @GetMapping("/pets")
+//    public getFilteredPets(@Param()) {
+//
+//    }
+    //get all pets from user given by id
+    //should be in user
+    @GetMapping("/users/{id}/pets")
+    public List<Pet> getPetsByUserId(@PathVariable long id) {
+        return petRepository.findByUserId(id);
+    }
+
+    //get user info from pet id
+    @GetMapping("/pets/{id}/owner")
+    public User getPetParentById(@PathVariable long id) {
+        return petRepository.getById(id).getUser();
+    }
+
 }
