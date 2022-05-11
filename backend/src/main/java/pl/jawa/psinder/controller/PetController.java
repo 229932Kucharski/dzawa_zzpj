c3c74fd6e2d0b9e25d954b172e90989776bdd42a
Krzysztof Dwużnik <krzysztofd_1999@wp.pl>
2022-05-11 08:48:55 +0200

    Modified adding and updating pets, added PetDto and PetService

--- backend/src/main/java/pl/jawa/psinder/controller/PetController.java
+++ backend/src/main/java/pl/jawa/psinder/controller/PetController.java
@@ -1,99 +1,93 @@
 package pl.jawa.psinder.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
+import pl.jawa.psinder.dto.PetDto;
 import pl.jawa.psinder.entity.Pet;
+import pl.jawa.psinder.entity.PetAddress;
 import pl.jawa.psinder.entity.User;
-import pl.jawa.psinder.repository.PetRepository;
-import pl.jawa.psinder.webclient.AddressDistanceClient;
+import pl.jawa.psinder.service.PetService;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 public class PetController {
 
-    private final PetRepository petRepository;
-
-    public PetController(PetRepository petRepository) {
-        this.petRepository = petRepository;
-    }
+    private final PetService petService;
 
     //get all pets from petRepository
     @GetMapping("/pets")
     public List<Pet> getPets() {
-        return petRepository.findAll();
+        return petService.getPets();
     }
 
     //get pet
     @GetMapping("/pets/{id}")
-    public Pet getPetById(@PathVariable long id) {
-        return petRepository.getById(id);
+    public Optional<Pet> getPetById(@PathVariable long id) {
+        return petService.getPet(id);
     }
 
     //create pet
     @PostMapping("/pets")
-    public Pet createPet(@RequestBody Pet pet) {
-        return petRepository.save(pet);
+    public Pet createPet(@RequestBody PetDto petDto) {
+        return petService.addPet(new Pet(
+                null,
+                petService.getOwner(1), //needs to change
+                petDto.getName(),
+                petDto.getRace(),
+                petDto.getSize(),
+                petDto.getDescription(),
+                null, //needs to change
+                petService.getPet(1).get().getAddress() //needs to change
+        ));
     }
 
     //update pet
     @PutMapping("/pets/{id}")
-    public Pet UpdatePet(@PathVariable long id, @RequestBody Pet pet) {
-        pet.setId(id);
-        return petRepository.save(pet);
+    public Pet updatePet(@PathVariable long id, @RequestBody PetDto petDto) {
+        return petService.updatePet(new Pet(
+                id,
+                petService.getOwner(id),
+                petDto.getName(),
+                petDto.getRace(),
+                petDto.getSize(),
+                petDto.getDescription(),
+                petService.getPet(id).get().getAvailabilities(),
+                petService.getPet(id).get().getAddress()
+        ));
     }
 
     //delete pet with given id
     @DeleteMapping("/pets/{id}")
     public void deletePet(@PathVariable long id) {
-        petRepository.deleteById(id);
+        petService.deletePet(id);
     }
 
     //get pet with filters
     @GetMapping("/pets/filtered")
-    public List<Pet> getFilteredPets(@Param("race") String race, @Param("size") String size, @Param("city") String city, @Param("street") String street, @Param("distance") double distance) {
-        List<Pet> result = petRepository.findAll();
-        if (race != null) {
-            result = result.stream().filter(pet -> pet.getRace().equals(race)).toList();
-        }
-        if (size != null) {
-            result = result.stream().filter(pet -> pet.getSize().equals(size)).toList();
-        }
-        if (city !=null) {
-            if (street !=null) {
-                String address = street + ", " + city;
-                AddressDistanceClient client = new AddressDistanceClient();
-                result = result.stream().filter(pet ->
-                        {
-                            String tmp = pet.getAddress().getStreet()
-                                    + ", " + pet.getAddress().getCity();
-                            try {
-                                double dist = client.getDistance(address, tmp);
-                                return dist <= distance;
-                            } catch (JsonProcessingException e) {
-                                return false;
-                            }
-                        }).toList();
-            } else {
-                result = result.stream().filter(pet -> pet.getAddress().getCity().equals(city)).toList();
-            }
-        }
-        return result;
+    public List<Pet> getFilteredPets(@Param("race") String race,
+                                     @Param("size") String size,
+                                     @Param("city") String city,
+                                     @Param("street") String street,
+                                     @Param("distance") double distance) {
+        return petService.getFilteredPets(race, size, city, street, distance);
     }
 
     //get all pets from user given by id
     //should be in user
     @GetMapping("/users/{id}/pets")
     public List<Pet> getPetsByUserId(@PathVariable long id) {
-        return petRepository.findByUserId(id);
+        return petService.getUsersPets(id);
     }
 
     //get user info from pet id
+    //CHANE TO userDto
     @GetMapping("/pets/{id}/owner")
     public User getPetParentById(@PathVariable long id) {
-        return petRepository.getById(id).getUser();
+        return petService.getOwner(id);
     }
 
 }
