8956d426b3a0d34dd064b38d6ea980e16967b0e1
229923Kowner <229923@edu.p.lodz.pl>
2022-05-18 17:58:37 +0200

    Added create

--- backend/src/main/java/pl/jawa/psinder/controller/ConnectionController.java
+++ backend/src/main/java/pl/jawa/psinder/controller/ConnectionController.java
@@ -1,10 +1,14 @@
 package pl.jawa.psinder.controller;
 
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.jawa.psinder.entity.Connection;
 import pl.jawa.psinder.repository.ConnectionRepository;
 
+import java.rmi.ServerException;
 import java.util.List;
 
 @RestController
@@ -35,5 +39,17 @@ public class ConnectionController {
     @GetMapping(value = "", params = "status")
     public List<Connection> getConnectionsByStatus(@RequestParam("status") String status) {
         return connectionRepository.findByStatus(status);
+    }
+
+    @PostMapping(path = "create",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Connection> create(@RequestBody Connection newConnection) {
+        Connection connection = connectionRepository.save(newConnection);
+        if (connection == null) {
+            return new ResponseEntity<>(connection, HttpStatus.METHOD_FAILURE);
+        } else {
+            return new ResponseEntity<>(connection, HttpStatus.CREATED);
+        }
     }
 }
