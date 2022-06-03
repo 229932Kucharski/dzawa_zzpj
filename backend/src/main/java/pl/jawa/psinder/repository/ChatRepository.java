b6810d7ef2b54f3dbb9e08486c0c0f9a8cfb5839
Untouched <paulipap12@gmail.com>
2022-06-03 14:43:57 +0200

    Deleted @Query annotation

--- backend/src/main/java/pl/jawa/psinder/repository/ChatRepository.java
+++ backend/src/main/java/pl/jawa/psinder/repository/ChatRepository.java
@@ -15,6 +15,6 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
 //    @Query(value = "SELECT * FROM chat WHERE user_id = :userId", nativeQuery = true)
     List<Chat> findChatsByUserId(@Param("id") long userId);
 
-    @Query(value = "SELECT * FROM chat WHERE connection_id = :connectionId", nativeQuery = true)
+//    @Query(value = "SELECT * FROM chat WHERE connection_id = :connectionId", nativeQuery = true)
     List<Chat> findChatsByConnectionId(@Param("id") long connectionId);
 }
