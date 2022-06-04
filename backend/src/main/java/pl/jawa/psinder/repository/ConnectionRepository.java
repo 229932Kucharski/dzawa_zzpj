5c5c4bb407dedd6d76e318613fc46591794055bd
229923Kowner <229923@edu.p.lodz.pl>
2022-06-04 13:49:52 +0200

    Added to connection controller id getters with a keyword

--- backend/src/main/java/pl/jawa/psinder/repository/ConnectionRepository.java
+++ backend/src/main/java/pl/jawa/psinder/repository/ConnectionRepository.java
@@ -19,4 +19,23 @@ public interface ConnectionRepository extends JpaRepository<Connection, Long> {
 
     @Query (value = "SELECT * FROM connection WHERE owner_id = :ownerId AND status = :status", nativeQuery = true)
     List<Connection> findByStatus(@Param("ownerId") int ownerId, @Param("status") String status);
+
+    @Query (value = "SELECT connection.* " +
+            "FROM connection " +
+            "JOIN pet ON connection.pet_id = pet.id " +
+            "JOIN petaddress ON connection.pet_id = petaddress.pet_id " +
+            "WHERE walker_id = :walkerId AND " +
+            "(lower(pet.name) LIKE CONCAT('%',:keyword,'%') OR lower(pet.race) LIKE CONCAT('%',:keyword,'%') OR lower(petaddress.city) LIKE CONCAT('%',:keyword,'%') OR lower(petaddress.street) LIKE CONCAT('%',:keyword,'%')) " +
+            "ORDER BY field(status, 'accepted', 'waiting', 'canceled')", nativeQuery = true)
+    List<Connection> findByWalkerIdWithKeyWord(@Param("walkerId") int walkerId, @Param("keyword") String keyword);
+
+    @Query (value = "SELECT connection.* " +
+            "FROM connection " +
+            "JOIN pet ON connection.pet_id = pet.id " +
+            "JOIN petaddress ON connection.pet_id = petaddress.pet_id " +
+            "JOIN user ON connection.walker_id = user.id " +
+            "WHERE owner_id = :ownerId AND " +
+            "(lower(pet.name) LIKE CONCAT('%',:keyword,'%') OR lower(pet.race) LIKE CONCAT('%',:keyword,'%') OR lower(petaddress.city) LIKE CONCAT('%',:keyword,'%') OR lower(petaddress.street) LIKE CONCAT('%',:keyword,'%') OR lower(user.firstname) LIKE CONCAT('%',:keyword,'%') OR lower(user.lastname) LIKE CONCAT('%',:keyword,'%') OR lower(user.username) LIKE CONCAT('%',:keyword,'%')) " +
+            "ORDER BY field(status, 'accepted', 'waiting', 'canceled')", nativeQuery = true)
+    List<Connection> findByOwnerIdWithKeyWord(@Param("ownerId") int ownerId, @Param("keyword") String keyword);
 }
